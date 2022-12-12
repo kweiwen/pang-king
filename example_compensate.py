7b31ef97f664816d751c2b86f8d25dc8b15f640e
kweiwen <kweiwen.tseng@gmail.com>
2022-12-12 11:28:37 +0800

    refactor all

--- example_compensate.py
+++ example_compensate.py
@@ -3,13 +3,37 @@ import matplotlib.pyplot as plt
 from material import *
 from multibands import *
 
+import numpy as np
+import struct
+
+def float_to_byte_array_little(input_data):
+    return list(struct.pack('<f', input_data))
+
+def byte_to_string(input_data):
+    output_data = ""
+    for data in input_data:
+        output_data += ('{:02x}'.format(data))
+    return output_data
+
+def convert_all(input_data):
+    output_data = ""
+    for data in input_data:
+        output_data += byte_to_string(float_to_byte_array_little(data))
+    # print(len(output_data))
+    # print(output_data)
+    return output_data
+
 def example():
-    room_size = [8, 3, 2.8]
-    microphone_pos = [7, 1.5, 1.55]
-    source_pos = [1.2, 1.5, 1.2]
+    room_size       = [3.2, 6, 2.7]
+    microphone_pos  = [1.1, 3, 1.2]
+    source_pos      = [2, 3, 2]
+    sample_rate     = 48000
+    velocity        = 340
+    sample_length   = 2048
+    block_size      = 240
 
     instance = ISM()
-    instance.define_system(48000, 343, 2048 - 256, 0.008)
+    instance.define_system(sample_rate, velocity, sample_length, 0.0025)
     instance.create_multi_bands()
     instance.create_room(room_size)
 
@@ -18,12 +42,12 @@ def example():
     # reverb_chamber
 
     m = dict()
-    m["floor"] =    Material(energy_absorption="carpet_tufted_9.5mm", scattering="rect_prism_boxes")
-    m["ceiling"] =  Material(energy_absorption="carpet_tufted_9.5mm", scattering="rect_prism_boxes")
-    m["east"] =     Material(energy_absorption="carpet_tufted_9.5mm", scattering="rect_prism_boxes")
-    m["west"] =     Material(energy_absorption="carpet_tufted_9.5mm", scattering="rect_prism_boxes")
-    m["north"] =    Material(energy_absorption="carpet_tufted_9.5mm", scattering="rect_prism_boxes")
-    m["south"] =    Material(energy_absorption="carpet_tufted_9.5mm", scattering="rect_prism_boxes")
+    m["floor"] =    Material(energy_absorption="audience_orchestra_choir", scattering="rect_prism_boxes")
+    m["ceiling"] =  Material(energy_absorption="audience_orchestra_choir", scattering="rect_prism_boxes")
+    m["east"] =     Material(energy_absorption="hard_surface", scattering="rect_prism_boxes")
+    m["west"] =     Material(energy_absorption="hard_surface", scattering="rect_prism_boxes")
+    m["north"] =    Material(energy_absorption="hard_surface", scattering="rect_prism_boxes")
+    m["south"] =    Material(energy_absorption="hard_surface", scattering="rect_prism_boxes")
 
     x1 = instance.resample(m['west'].energy_absorption['coeffs'], m['west'].energy_absorption['center_freqs'])
     x2 = instance.resample(m['east'].energy_absorption['coeffs'], m['east'].energy_absorption['center_freqs'])
@@ -39,6 +63,28 @@ def example():
     instance.compute_ism()
     instance.compute_rir()
 
+
+    # maximum_length(M + N - 1) to store rir coefficient for circular convolution
+    maximum_length = len(instance.tap[:sample_length + 1 - block_size])
+    padding_length = block_size - 1
+    padding = np.zeros(padding_length)
+    temp = np.concatenate((instance.tap[:maximum_length], padding)) * instance.compute_engery_scale()
+
+    A = np.zeros(2048 * 2)
+
+    H = np.fft.fft(temp)
+
+    for index, data in enumerate(H):
+        A[index*2    ] = data.real
+        A[index*2 + 1] = data.imag
+
+    result = convert_all(A)
+    print(result)
+    print(len(result))
+
+    np.savetxt('impedance_1.dat', [temp], delimiter=',\n', fmt='%.24f')
+
+def render_plot(instance):
     fig, axs = plt.subplots(2, 2, constrained_layout=True)
     fig.suptitle('Original vs Compensated')
 
@@ -77,10 +123,6 @@ def example():
     ax2.set_ylim(-np.pi, np.pi)
 
     plt.show()
-
-    # np.savetxt('impedance_1.dat', [instance.tap * instance.compute_engery_scale()], delimiter=',\n', fmt='%.24f')
-
-
 
 if __name__ == "__main__":
     example()
