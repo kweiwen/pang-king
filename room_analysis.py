091744d5de53ca63f760420e71b4669c5fdf330b
COMPANY\lixin25 <lixin25@lixiang.com>
2023-02-17 16:24:19 +0800

    modify room_analysis script

--- room_analysis.py
+++ room_analysis.py
@@ -14,29 +14,42 @@ def nextpow2(n):
 def _Integral(array):
     return np.flipud(np.cumsum(np.flipud(array)))
 
-# calculate EDR
-def _EnergyDecayRelief(inpath, fs=48000):
-    if type(inpath) == str:
-        w = wavio.read(inpath)
-        data = w.data[:, 0] / (2 ** (w.sampwidth * 8 - 1) - 1)
-        fs = w.rate
+def PreProcessData(xn, frameSize, stepSize):
+    padding = len(xn) % stepSize
+    if(padding == 0):
+        temp = xn
     else:
-        data = inpath
+        temp = np.append(xn, np.zeros(stepSize - padding))
 
-    # Settings and Calculating STFT
-    frameSizeMs = 20  # minimum frame length, in ms
-    overlap = 0.75  # fraction of frame overlapping
-    minFrameLen = fs * frameSizeMs / 1000
-    frameLenPow = nextpow2(minFrameLen)
-    frameLen = 2 ** frameLenPow  # frame length = fft size
-    F, T, B = signal.stft(data, fs, 'hann', frameLen, overlap * frameLen)
+    data = np.append(np.zeros(frameSize - stepSize), temp)
 
-    # Calculating Energy
-    B_energy = np.multiply(B, B.conjugate())
-    B_EDR = np.zeros((np.size(B, 0), np.size(B, 1)))
-    for i in range(0, np.size(B, 0)):
-        B_EDR[i, :] = _Integral(B_energy[i, :])
-    return T, F, B_EDR
+    iteration = (len(data) // stepSize) - (frameSize // stepSize) + 1
+
+    dataSet = np.zeros([iteration, frameSize])
+
+    for i in range(iteration):
+        start = i * stepSize
+        end = start + frameSize
+        dataSet[i] = data[start:end]
+
+    return dataSet, data, len(data), iteration
+
+# calculate EDR
+def _EnergyDecayRelief(dataSet, iteration, N):
+    fftSet = np.zeros([N, iteration])
+    datalen = len(dataSet[0])
+    winfunc = np.hanning(datalen)
+    for i in range(iteration):
+        dataSet[i] *= winfunc
+        fftSet[:, i] = np.fft.fft(dataSet[i], N)
+
+    B_Amp = np.abs(fftSet) / N * 2
+    B_AmpSum = np.zeros([int(N / 2) + 1, iteration])
+
+    for i in range(np.size(B_AmpSum, 0)):
+        B_AmpSum[i, :] = _Integral(B_Amp[i, :])
+    B_EDR = B_AmpSum ** 2
+    return B_EDR
 
 # calculate EDC
 def _EnergyDecayCurve(inpath, fs=48000):
@@ -58,9 +71,11 @@ Example: Input a IR File
 """
 if __name__ == "__main__":
     path = 'D:/lixin25/Download/Eric Leung Acoustic Guitar IRs/ACgtr IRs/Ac Gtr Verb st.wav'
-
+    w = wavio.read(path)
+    IRdata = w.data[:, 0] / (2 ** (w.sampwidth * 8 - 1) - 1)
+    fs = w.rate
     # Plot EDC
-    T, B_EDC = _EnergyDecayCurve(path)
+    T, B_EDC = _EnergyDecayCurve(IRdata)
 
     B_EDCdb = 10.0 * np.log10(B_EDC)  # Convert to Decibel Representation
     offset = np.max(B_EDCdb)
@@ -78,26 +93,36 @@ if __name__ == "__main__":
     plt.show()
 
     # Plot EDR
-    T, F, B_EDR = _EnergyDecayRelief(path)
+    frameSizeMs = 20
+    overlap = 0.75
+    minFrameLen = int(fs * frameSizeMs / 1000)
+    frameLenPow = nextpow2(minFrameLen)
+    N = 2 ** frameLenPow
+    StepSize = int((1 - overlap) * minFrameLen)
 
-    B_EDRdb = 10 * np.log10(B_EDR)  # Convert to Decibel Representation
-    offset = np.max(B_EDRdb)
-    B_EDRdbN = B_EDRdb - offset  # Normalized
-    B_EDRdbN_trunc = B_EDRdbN
-    minPlotDB = -70  # Set Threshold
-    for i in range(0, np.size(B_EDRdbN, 0)):
-        for j in range(0, np.size(B_EDRdbN, 1)):
-            if (B_EDRdbN_trunc[i, j] < minPlotDB):
-                B_EDRdbN_trunc[i, j] = minPlotDB
+    dataSet, data, datalen, iteration = PreProcessData(IRdata, minFrameLen, StepSize)
 
-    X, Y = np.meshgrid(T * 1000, F / 1000)
-    Z = B_EDRdbN_trunc
+    B_EDR = _EnergyDecayRelief(dataSet, iteration, N)
+    B_EDRdB = 10 * np.log10(B_EDR)
+    minPlotDB = -80  # Set Threshold
+    for i in range(0, np.size(B_EDRdB, 0)):
+        for j in range(0, np.size(B_EDRdB, 1)):
+            if (B_EDRdB[i, j] < minPlotDB):
+                B_EDRdB[i, j] = minPlotDB
+
+    k = np.linspace(0, int(N / 2), int(N / 2) + 1)
+    fk = k / N * fs
+    t = np.linspace(0, datalen / fs, iteration)
+
+    X, Y = np.meshgrid(t * 1000, fk / 1000)
+    Z = B_EDRdB
     c = plt.pcolormesh(X, Y, Z, cmap='jet')  # Two-dimensional
     plt.colorbar(c)
     plt.title(' Energy Decay Relief of IR')
     plt.ylabel('frequency(kHz)')
     plt.xlabel('time(ms)')
     plt.show()
+
 
     fig = plt.figure()
     ax = plt.axes(projection='3d')
