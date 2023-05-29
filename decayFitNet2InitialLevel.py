da1efd7ae2f52233a42cf6c70e707204b27fbd8c
kweiwen.tseng <kweiwen.tseng@gmail.com>
2023-05-29 17:44:16 +0800

    update GEQ

--- decayFitNet2InitialLevel.py
+++ decayFitNet2InitialLevel.py
@@ -1,27 +1,3 @@
-# function [level, A, N] = decayFitNet2InitialLevel(T, A, N, normalization, fs, rirLen, fBands)
-# % convert decayFitNet estimation to initial level as used in FDNs
-# %
-# % (c) Sebastian J. Schlecht, Saturday, 28 January 2023
-#
-# % Denormalize the amplitudes of the EDC
-# A = A .* normalization;
-# N = N .* normalization;
-#
-# % Estimate the energy of the octave filters
-# rirFBands = octaveFiltering([1; zeros(fs,1)], fs, fBands);
-# bandEnergy = sum(rirFBands.^2,1);
-#
-# % Cumulative energy is a geometric series of the gain per sample
-# gainPerSample = db2mag(RT602slope(T, fs));
-# decayEnergy = 1 ./ (1 - gainPerSample.^2);
-#
-# % initial level
-# level = sqrt( A ./ bandEnergy ./ decayEnergy * rirLen);
-# % there is an offset because, the FDN is not energy normalized
-# % The rirLen factor is due to the normalization in schroederInt (in DecayFitNet)
-#
-# end
-
 import numpy as np
 import sys
 from scipy.signal import butter, sosfilt, zpk2sos
@@ -79,6 +55,75 @@ def decayFitNet2InitialLevel(T, A, N, normalization, fs, rirLen, fBands):
     level = np.sqrt( A_norm / bandEnergy / decayEnergy * rirLen)
 
     return level, A_norm, N_norm
+
+def graphicEQ(centerOmega, shelvingOmega, R, gaindB):
+    numFreq = len(centerOmega) + len(shelvingOmega) + 1
+    assert len(gaindB) == numFreq
+    SOS = np.zeros((numFreq, 6))
+
+    for band in range(numFreq):
+        if band == 0:
+            b = [1, 0, 0] * db2mag(gaindB[band])
+            a = [1, 0, 0]
+        elif band == 1:
+            b, a = shelvingFilter(shelvingOmega[0], db2mag(gaindB[band]), 'low')
+        elif band == numFreq - 1:
+            b, a = shelvingFilter(shelvingOmega[1], db2mag(gaindB[band]), 'high')
+        else:
+            Q = np.sqrt(R) / (R - 1)
+            b, a = bandpassFilter(centerOmega[band - 2], db2mag(gaindB[band]), Q)
+
+        sos = np.hstack((b, a))
+
+        SOS[band, :] = sos
+    return SOS
+
+
+def shelvingFilter(omegaC, gain, Q):
+    b = np.zeros(3)
+    a = np.zeros(3)
+
+    t = np.tan(omegaC / 2)
+    t2 = t ** 2
+    g2 = gain ** 0.5
+    g4 = gain ** 0.25
+
+    b[0] = g2 * t2 + np.sqrt(2) * t * g4 + 1
+    b[1] = 2 * g2 * t2 - 2
+    b[2] = g2 * t2 - np.sqrt(2) * t * g4 + 1
+
+    a[0] = g2 + np.sqrt(2) * t * g4 + t2
+    a[1] = 2 * t2 - 2 * g2
+    a[2] = g2 - np.sqrt(2) * t * g4 + t2
+
+    b = g2 * b
+
+    if type == 'low':
+        pass
+    elif type == 'high':
+        tmp = b
+        b = a * gain
+        a = tmp
+
+    return b, a
+
+
+def bandpassFilter(omegaC, gain, Q):
+    b = np.zeros(3)
+    a = np.zeros(3)
+
+    bandWidth = omegaC / Q
+    t = np.tan(bandWidth / 2)
+
+    b[0] = np.sqrt(gain) + gain * t
+    b[1] = -2 * np.sqrt(gain) * np.cos(omegaC)
+    b[2] = np.sqrt(gain) - gain * t
+
+    a[0] = np.sqrt(gain) + t
+    a[1] = -2 * np.sqrt(gain) * np.cos(omegaC)
+    a[2] = np.sqrt(gain) - t
+
+    return b, a
 
 
 if __name__ == "__main__":
