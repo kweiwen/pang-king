dc766880f41bd5281357a6611df3bfda5079c6ea
kweiwen.tseng <kweiwen.tseng@gmail.com>
2023-05-31 11:28:00 +0800

    refactor array

--- external.py
+++ external.py
@@ -1,7 +1,9 @@
 import numpy as np
 import sys
+import wavio
 from scipy.signal import butter, sosfilt, zpk2sos, freqz
 from scipy.optimize import lsq_linear
+from DecayFitNet.python.toolbox.DecayFitNetToolbox import DecayFitNetToolbox
 
 def mag2db(input_data):
     return 20*np.log10(input_data)
@@ -45,6 +47,7 @@ def octaveFiltering(inputSignal, fs, fBands):
     return outBands
 
 def decayFitNet2InitialLevel(T, A, N, normalization, fs, rirLen, fBands):
+    normalization = np.array(normalization)
     A_norm = A * normalization
     N_norm = N * normalization
 
@@ -193,44 +196,37 @@ def designGEQ(targetG: np.array):
     return sos, targetF
 
 
-def RIR2FilterCoeff(fp, DelayLine, fs):
-    # Load RIR file
-    rir = []
-    for line in fp:
-        line = line.strip('\n')
-        rir.append(line.split(' '))
-    fp.close()
-    rir = np.array(rir, dtype=float)
-
-    N = 4
+def RIR2AbsCoefLvlCoef(data, delayLines, fs):
     n_slopes = 1
     filter_frequencies = [63, 125, 250, 500, 1000, 2000, 4000, 8000]
 
     # Prepare the model
-    IRnet = DecayFitNetToolbox(n_slopes=n_slopes, sample_rate=fs, filter_frequencies=filter_frequencies)
-    est_parameters_net, norm_vals_net = IRnet.estimate_parameters(rir, analyse_full_rir=True)
-    est_T = est_parameters_net[0].T
-    est_A = est_parameters_net[1].T
-    est_N = est_parameters_net[2].T
-    est_L, est_A, est_N = decayFitNet2InitialLevel(est_T, est_A, est_N, norm_vals_net, fs, rir.shape[0], filter_frequencies)
+    net = DecayFitNetToolbox(n_slopes=n_slopes, sample_rate=fs, filter_frequencies=filter_frequencies)
+    est_parameters_net, norm_vals_net = net.estimate_parameters(data, analyse_full_rir=True)
+    estT = est_parameters_net[0].T
+    estA = est_parameters_net[1].T
+    estN = est_parameters_net[2].T
+    estL, estA, estN = decayFitNet2InitialLevel(estT, estA, estN, norm_vals_net, fs, data.shape[0], filter_frequencies)
 
-    targetT60 = np.hstack((est_T[0, 0], est_T[0], est_T[0, -1]))
+    targetT60 = np.hstack((estT[0, 0], estT[0], estT[0, -1]))
     targetT60 = np.multiply(targetT60, np.array([0.9, 1, 1, 1, 1, 1, 1, 1, 0.9, 0.5]))
 
     # Convert T60 to magnitude response
     targetG = RT602slope(targetT60, fs)
 
-    BiquadFilterCoeff = np.ones((5 * 5 * 11, 1))
+    output_data = np.zeros([len(delayLines)+1, len(filter_frequencies)+3, 6])
 
-    for i in range(N):
-        BiquadFilterCoeff[11 * 5 * i:11 * 5 * (i + 1), :] = designGEQ(DelayLine[i] * targetG)
+    for index, delayLine in enumerate(delayLines):
+        sos, _ = designGEQ(delayLine * targetG)
+        output_data[index] = sos
 
-    estLevel = np.hstack((est_L[0, 0], est_L[0], est_L[0, -1]))
+    estLevel = np.hstack((estL[0, 0], estL[0], estL[0, -1]))
     targetLevel = mag2db(estLevel)
     targetLevel = targetLevel - np.array([5, 0, 0, 0, 0, 0, 0, 0, 5, 30])
-    BiquadFilterCoeff[11 * 5 * N:11 * 5 * (N + 1), :] = designGEQ(targetLevel)
+    sos, _ = designGEQ(targetLevel)
+    output_data[-1] = sos
 
-    return BiquadFilterCoeff
+    return output_data
 
 
 def demo_decayFitNet2InitialLevel():
@@ -238,25 +234,32 @@ def demo_decayFitNet2InitialLevel():
     fBands = [0, 500, 1000, 2000, 4000, 8000, 16000, fs/2]
     rirLen = fs
 
-    impulse = np.random.ranf(fs + 1)
-    # impulse = np.zeros(fs+1)
-    # RIRs = octaveFiltering(impulse, fs, fBands).T
-    #
-    # for RIR in RIRs:
-    #     magnitude = np.abs(np.fft.rfft(RIR))
-    #     fn = np.log10(magnitude/len(RIR)) * 20
-    #     plt.plot(fn)
-    # plt.show()
-
     normalization = np.random.ranf(8)
     T = np.random.ranf(8)
     A = np.random.ranf(8)
     N = np.random.ranf(8)
     level, A_norm, N_norm = decayFitNet2InitialLevel(T, A, N, normalization, fs, rirLen, fBands)
 
-    # print(level)
-    # print(A_norm)
-    # print(N_norm)
+    print(level)
+    print(A_norm)
+    print(N_norm)
+
+def demo():
+    fp = "C:\Python37\Lib\DecayFitNet\data\exampleRIRs\singleslope_0001_1_sh_rirs.wav"
+    file = wavio.read(fp)
+    data = file.data[:, 0] / (2 ** (file.sampwidth * 8 - 1) - 1)
+    print(data)
 
 if __name__ == "__main__":
-    designGEQ([0.99, 0.9, 0.9, 0.9, 0.99, 0.9, 0.9, 0.9, 0.85, 0.75])
+
+    fp = "C:\Python37\Lib\DecayFitNet\data\exampleRIRs\singleslope_0001_1_sh_rirs.wav"
+    file = wavio.read(fp)
+    data = file.data[:, 0] / (2 ** (file.sampwidth * 8 - 1) - 1)
+    delayLines = np.array([1021, 2029, 3001, 4093])
+    BiquadFilterCoeff = RIR2AbsCoefLvlCoef(data, delayLines, file.rate)
+    print(BiquadFilterCoeff)
+
+    # designGEQ([0.99, 0.9, 0.9, 0.9, 0.99, 0.9, 0.9, 0.9, 0.85, 0.75])
+
+
+
